1c8019128d1df03e23f56e18dda093501dd10cde
Yannis Guedel <yannis.guedel@unic.com>
2015-03-30 11:01:36 +0200

    added updatedatabase pipeline processor

---
+++ src/Unic.Bob.Scrambler.Core/Security/SecurityState.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Unic.Bob.Scrambler.Core.Security
+{
+    using System.Configuration;
+    using System.Web;
+
+    using Sitecore.Security.Authentication;
+
+    public class SecurityState
+    {
+        public SecurityState()
+        {
+            var user = AuthenticationManager.GetActiveUser();
+
+            if (user.IsAdministrator)
+            {
+                IsAllowed = true;
+                IsAutomatedTool = false;
+            }
+            else
+            {
+                var authToken = HttpContext.Current.Request.Headers["Authenticate"];
+                var correctAuthToken = ConfigurationManager.AppSettings["DeploymentToolAuthToken"];
+
+                if (!string.IsNullOrWhiteSpace(correctAuthToken) && !string.IsNullOrWhiteSpace(authToken)
+                    && authToken.Equals(correctAuthToken, StringComparison.Ordinal))
+                {
+                    IsAllowed = true;
+                    IsAutomatedTool = true;
+                }
+                else
+                {
+                    IsAllowed = false;
+                    IsAutomatedTool = false;
+                }
+            }
+        }
+
+        public bool IsAllowed { get; private set; }
+        public bool IsAutomatedTool { get; private set; }
+    }
+}
