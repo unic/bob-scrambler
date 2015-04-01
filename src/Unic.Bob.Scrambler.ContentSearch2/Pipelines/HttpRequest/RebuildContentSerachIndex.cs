a63387e4dd056c3d7370cccfe2776739ce2b6db4
Yannis Guedel <yannis.guedel@unic.com>
2015-04-01 10:02:06 +0200

    Implemented Scrambler

---
+++ src/Unic.Bob.Scrambler.ContentSearch2/Pipelines/HttpRequest/RebuildContentSerachIndex.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unic.Bob.Scrambler.ContentSearch.Pipelines.HttpRequest
+{
+    using System.Web;
+
+    using Sitecore.ContentSearch;
+
+    using Unic.Bob.Scrambler.Core.Pipelines.HttpRequest;
+
+    public class RebuildContentSerachIndex : RebuildIndex
+    {
+        public RebuildContentSerachIndex(string activationUrl)
+            : base(activationUrl)
+        {
+        }
+
+        protected override void ProcessRequest(HttpContext context)
+        {
+            base.ProcessRequest(context);
+
+            context.Response.Write("New");
+            foreach (var index in ContentSearchManager.Indexes)
+            {
+                context.Response.Write(index.Name);
+
+            }
+        }
+    }
+}
--- src/Unic.Bob.Scrambler.Core/Pipelines/HttpRequest/FullPublish.cs
+++ src/Unic.Bob.Scrambler.Core/Pipelines/HttpRequest/FullPublish.cs
@@ -1,44 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Unic.Bob.Scrambler.Core.Pipelines.HttpRequest
+﻿namespace Unic.Bob.Scrambler.Core.Pipelines.HttpRequest
 {
+    using System;
     using System.Web;
 
-    using Sitecore.Data;
+    using Sitecore.Configuration;
     using Sitecore.Globalization;
     using Sitecore.Publishing;
 
+    /// <summary>
+    /// Pipeline processor for exposing a HTTP service to perform a full publish.
+    /// </summary>
     public class FullPublish : BasePipelineProcessor
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FullPublish" /> class.
+        /// </summary>
+        /// <param name="activationUrl">The url where the pipline processor responds to. "/bob/" will always be prefixed to the passed url.</param>
         public FullPublish(string activationUrl)
             : base(activationUrl)
         {
         }
 
+        /// <summary>
+        /// Processes the HTTP request and performs a full publish.
+        /// </summary>
+        /// <param name="context">The context of the HTTP request.</param>
         protected override void ProcessRequest(HttpContext context)
         {
-            var webDb = Sitecore.Configuration.Factory.GetDatabase("web");
-            var masterDb = Sitecore.Configuration.Factory.GetDatabase("master");
+            var webDb = Factory.GetDatabase("web");
+            var masterDb = Factory.GetDatabase("master");
             foreach (Language language in masterDb.Languages)
             {
-                //loops on the languages and do a full republish on the whole sitecore content tree
                 var options = new PublishOptions(masterDb, webDb, PublishMode.Full, language, DateTime.Now)
                                   {
-                                      RootItem
-                                          =
-                                          masterDb
-                                          .Items
-                                          [
-                                              "/sitecore"
-                                          ],
-                                      RepublishAll
-                                          =
-                                          true,
-                                      Deep =
-                                          true
+                                      RootItem = masterDb.Items["/sitecore"],
+                                      RepublishAll = true,
+                                      Deep = true
                                   };
 
                 var myPublisher = new Publisher(options);
