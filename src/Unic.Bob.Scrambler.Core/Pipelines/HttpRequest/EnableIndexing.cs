99ac36d3072e992f2e5d1a4d24bc8092ef8354f7
Yannis Guedel <yannis.guedel@unic.com>
2015-04-14 11:19:08 +0200

    add disable/enable indexing

---
+++ src/Unic.Bob.Scrambler.Core/Pipelines/HttpRequest/EnableIndexing.cs
@@ -0,0 +1,27 @@
+﻿namespace Unic.Bob.Scrambler.Core.Pipelines.HttpRequest
+{
+    using System.Web;
+
+    public class ReEnableIndexing : BasePipelineProcessor
+    {
+        public static bool OrignalIndexingEnabled = Sitecore.Configuration.Settings.Indexing.Enabled;
+
+        public ReEnableIndexing(string activationUrl)
+            : base(activationUrl)
+        {
+        }
+
+        protected override void ProcessRequest(HttpContext context)
+        {
+            if (OrignalIndexingEnabled)
+            {
+                Sitecore.Configuration.Settings.Indexing.Enabled = true;
+                context.Response.Write("Re-enabled indexing.");
+            }
+            else
+            {
+                context.Response.Write("Indexing was already disabled at start, so no action is required.");
+            }
+        }
+    }
+}
