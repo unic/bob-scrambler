38d599970f349d2ec328946bd2a6726fef6b4478
Yannis Guedel <yannis.guedel@unic.com>
2015-07-07 11:43:18 +0200

    added functionality to reset admin password

--- docs/README.md
+++ docs/README.md
@@ -5,19 +5,9 @@ Scrambler provides web-services which can be called via PowerShell to
 perform actions inside Sitecore.
 
 ## Authorization
-Before you are allowed to access the API, Bob needs to approve your request.
-To do so you need to either be logged in as admin in Sitecore, or provide
-a valid value for the "Authenticate" HTTP header.
-To value of the "Authenticate" header must be equal to the "DeploymentToolAuthToken"
-app setting:
+The APIs can be called either if one is logged in as admin in Sitecore, or
+when the HTTP call comes from the local machine.
 
-    <add key="DeploymentToolAuthToken" value"MySecretValue" />
-
-You can then use the following PowerShell snippet to call an API:
-
-    Invoke-WebRequest http://local.bob.ch/bob/myEndPoint -Headers {"Authenticate" = "MySecretValue"}
-
-*Note:* `DeploymentToolAuthToken` is also used by Unicorn.
 
 ## API Endpoints
 Scrambler provides different API Endpoints, which are documented here.
---
+++ src/Unic.Bob.Scrambler.Core/Pipelines/HttpRequest/ResetAdminUser.cs
@@ -0,0 +1,37 @@
+﻿namespace Unic.Bob.Scrambler.Core.Pipelines.HttpRequest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Security;
+
+    using Sitecore.Search;
+
+    /// <summary>
+    /// Pipeline processor for exposing a HTTP service for rebuilding the search index.
+    /// </summary>
+    public class ResetAdminUser : BasePipelineProcessor
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResetAdminUser" /> class.
+        /// </summary>
+        /// <param name="activationUrl">The url where the pipline processor responds to. "/bob/" will always be prefixed to the passed url.</param>
+        public ResetAdminUser(string activationUrl)
+            : base(activationUrl)
+        {
+        }
+
+        /// <summary>
+        /// Processes the HTTP request by reseting the password of the admin user.
+        /// </summary>
+        /// <param name="context">The HTTP context of the request.</param>
+        protected override void ProcessRequest(HttpContext context)
+        {
+            MembershipUser  user = Membership.GetUser("sitecore\\admin", true);
+            var resetPassword = user.ResetPassword();
+            user.ChangePassword(resetPassword, "b");
+            context.Response.Write("Reseted admin password.");
+        }
+    }
+}
